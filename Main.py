8c3a3ea048cb4b2b578a46e1d2f5103dbba666dd
VattyTheHut <Heketavnolroc@gmail.com>
2016-10-02 21:22:33 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,8 +3,19 @@ import statistics
 import pandas
 
 
+#the X&Y axis 
+x = []
+y = []
+
+#This is the Dracula book and the process of opening and reading it. 
+#then afterwards splitting the book up prod by people
 file = open("feedbooks_book_88", "r")
 data = file.read()
 clean_data = data.split(".")
 
-for i in 
+#this code is to create the data point for the graph
+for i in range(len(clean_data)):
+  x.append(i)
+y = [sum(x == "dracula".lower() for x in i) for i in clean_data]
+
+
